5de97a8473e773ec8994848da93429181f7fba26
qunqingrihe <hxz2046084122@gmail.com>
2025-09-27 20:05:47 +0800

    feat: 个性化博客配置完成

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -9,10 +9,11 @@ export default function PoweredBy(props) {
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
       <a
-        href='https://github.com/tangly1024/NotionNext'
+        href='https://github.com/AsterZephyr'
         className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
+        Aster Zephyr
       </a>
+      <span className='text-xs text-gray-500 ml-2'>Go · LLM · Java · Rust</span>
       .
     </div>
   )
--- themes/fukasawa/components/SiteInfo.js
+++ themes/fukasawa/components/SiteInfo.js
@@ -41,10 +41,12 @@ function SiteInfo({ title }) {
         <span className='text-xs font-serif'>
           Powered by
           <a
-            href='https://github.com/tangly1024/NotionNext'
+            href='https://github.com/AsterZephyr'
             className='underline'>
-            NotionNext {siteConfig('VERSION')}
+            Aster Zephyr
           </a>
+          <br />
+          <span className='text-xs text-gray-500'>Go · LLM · Java · Rust</span>
         </span>
         <br />
       </span>
